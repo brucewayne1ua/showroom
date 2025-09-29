8b81d165190ceeafb61c4884e1f008c583b62b9e
Rustam Ohli <adamsonrustam@gmail.com>
2025-09-29 09:06:54 +0300

    add search algorythm

--- cars/urls.py
+++ cars/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
 
-app_name = 'cars'
+app_name = "cars"
 
 urlpatterns = [
-    path('', views.car_list, name='car_list'),              
-    path('add/', views.car_add, name='car_add'),        
-    path('order-thanks/', views.order_thanks, name='order_thanks'),
-    path('<int:pk>/', views.car_detail, name='car_detail'), 
-     path('<int:pk>/buy/', views.car_buy, name='car_buy'),
+    path("", views.car_list, name="car_list"),
+    path("filter/", views.car_filter, name="car_filter"),
+    path("<int:pk>/", views.car_detail, name="car_detail"),
+    path("add/", views.car_add, name="car_add"),
+    path("buy/<int:pk>/", views.car_buy, name="car_buy"),
+    path("thanks/", views.order_thanks, name="order_thanks"),
 ]
--- cars/views.py
+++ cars/views.py
@@ -6,18 +6,44 @@ from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 
 def car_list(request):
+    """Основная страница со списком авто"""
+    brands = Brand.objects.all()
+    return render(request, 'cars/car_list.html', {'brands': brands})
+
+def car_filter(request):
+    """Фильтрация (AJAX)"""
     qs = Car.objects.filter(status='available').select_related('brand')
+
     q = request.GET.get('q')
     brand = request.GET.get('brand')
+    price_min = request.GET.get('price_min')
+    price_max = request.GET.get('price_max')
+    year_min = request.GET.get('year_min')
+    year_max = request.GET.get('year_max')
+
     if q:
-        qs = qs.filter(Q(model__icontains=q) | Q(description__icontains=q) | Q(brand__name__icontains=q))
+        qs = qs.filter(
+            Q(model__icontains=q) |
+            Q(description__icontains=q) |
+            Q(brand__name__icontains=q)
+        )
     if brand:
         qs = qs.filter(brand__id=brand)
+    if price_min:
+        qs = qs.filter(price__gte=price_min)
+    if price_max:
+        qs = qs.filter(price__lte=price_max)
+    if year_min:
+        qs = qs.filter(year__gte=year_min)
+    if year_max:
+        qs = qs.filter(year__lte=year_max)
+
     paginator = Paginator(qs.order_by('-created_at'), 12)
     page = request.GET.get('page')
     cars = paginator.get_page(page)
-    brands = Brand.objects.all()
-    return render(request, 'cars/car_list.html', {'cars': cars, 'brands': brands})
+
+    return render(request, 'cars/car_list_results.html', {'cars': cars})
+
 
 def car_detail(request, pk):
     car = get_object_or_404(Car, pk=pk)
@@ -29,17 +55,16 @@ def car_detail(request, pk):
             if request.user.is_authenticated:
                 order.buyer = request.user
             order.save()
-            # можно отправить email / уведомление
             return redirect('cars:order_thanks')
     else:
         form = OrderForm()
-    return render(request, 'cars/car_detail.html', {'car':car, 'form':form})
+    return render(request, 'cars/car_detail.html', {'car': car, 'form': form})
 
 def order_thanks(request):
     return render(request, 'cars/order_thanks.html')
 
 def car_buy(request, pk):
-    car = get_object_or_404(Car, pk = pk)
+    car = get_object_or_404(Car, pk=pk)
     return render(request, 'cars/car_buy.html', {'car': car})
 
 @login_required
@@ -48,10 +73,9 @@ def car_add(request):
         form = CarForm(request.POST, request.FILES)
         if form.is_valid():
             car = form.save(commit=False)
-            car.owner = request.user  # привязываем авто к авторизованному юзеру
+            car.owner = request.user
             car.save()
             return redirect('cars:car_list')
     else:
         form = CarForm()
-
     return render(request, 'cars/car_add.html', {'form': form})
